dac5d008ac7f2213b37f25a7f0cff21e6772e95f
Priya Bhatt <pbhatt@sapient.com>
2018-12-13 11:45:59 +0530

    First Commit

---
+++ src/components/Bio.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+// Import typefaces
+import 'typeface-montserrat'
+import 'typeface-merriweather'
+
+import profilePic from './profile-pic.jpg'
+import { rhythm } from '../utils/typography'
+
+class Bio extends React.Component {
+  render() {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          marginBottom: rhythm(2.5),
+        }}
+      >
+        <img
+          src={profilePic}
+          alt={`Priya Bhatt`}
+          style={{
+            marginRight: rhythm(1 / 2),
+            marginBottom: 0,
+            width: rhythm(2),
+            height: rhythm(2),
+          }}
+        />
+        <p>
+          Written by <strong>Priya Bhatt</strong> who lives and works in Bangalore.{' '}
+          <a href="https://twitter.com/PriyaDBhatt">
+            You should follow her on Twitter
+          </a>
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Bio
---
+++ src/pages/gatsby-netlify-setup/index.md
@@ -0,0 +1,11 @@
+---
+title: GatsBy Netlify setup
+date: "2018-12-13"
+---
+
+This is my first post here.
+
+I'm sure I'll write a lot more interesting things in the future.
+
+This blog post is to test integration between gatsby and netlify.
+
